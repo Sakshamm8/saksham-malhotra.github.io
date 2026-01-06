db736db9513aed3b7d021d2db054756e7729544d
Sakshamm8 <saksham.malhotra18@gmail.com>
2026-01-05 23:05:02 -0500

    Updates

--- _next/static/B-59STg0kxiDPcHs-d7-m/_buildManifest.js
+++ _next/static/B-59STg0kxiDPcHs-d7-m/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/saksham-portfolio//_next/:path+",
+        "destination": "/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
